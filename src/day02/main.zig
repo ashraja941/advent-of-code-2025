d38780ababe6bddcf4b03738d9decaf6f25d1525
ash <ashraja941@gmail.com>
2025-12-02 12:33:17 -0600

    day 02 : part 2

--- src/day02/main.zig
+++ src/day02/main.zig
@@ -13,11 +13,26 @@ fn findInvalidInRange(start: u64, end: u64) !u64 {
         var buffer: [30]u8 = undefined;
         const str = try std.fmt.bufPrint(&buffer, "{d}", .{i});
         const len: u8 = @intCast(str.len);
-        const half: u8 = @divFloor(len, 2);
+        // const half: u8 = @divFloor(len, 2);
 
-        if (std.mem.eql(u8, str[0..half], str[half..])) {
-            numInvalid += @intCast(i);
-            print("invalid : {d}\n", .{i});
+        for (2..len + 1) |parts| {
+            const partInt: u8 = @intCast(parts);
+            if (len % partInt != 0) continue;
+            const chunk: u8 = @intCast(len / parts);
+            var invalidBool: bool = true;
+
+            for (1..parts) |part| {
+                const partStart: u8 = @intCast(part * chunk);
+                if (!std.mem.eql(u8, str[partStart .. partStart + chunk], str[0..chunk])) {
+                    invalidBool = false;
+                    break;
+                }
+            }
+            if (invalidBool) {
+                numInvalid += @intCast(i);
+                print("invalid : {d}\n", .{i});
+                break;
+            }
         }
     }
     return numInvalid;
